8fb0dba14fae6773370ed6e597d204a34456198a
Diogo Monteiro <diogo.gmt@gmail.com>
2011-09-26 12:27:11 -0400

    bth540 lab 1

---
+++ bth540/js/script.js
@@ -0,0 +1,6 @@
+$(function(){ 							   
+
+	// radiys box	
+	$('.menu_nav ul li a').css({"border-radius": "5px 5px 0 0", "-moz-border-radius":"5px 5px 0 0", "-webkit-border-radius":"5px 5px 0 0"});
+	
+});	
