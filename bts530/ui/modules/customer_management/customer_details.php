b649551380a40a2a99100084dc519ba7eb0afb57
Diogo Golovanevsky Monteiro <diogo.gmt@gmail.com>
2011-10-04 01:00:25 -0400

    Add customer_details / customer_management / calendar / gallery

---
+++ bts530/ui/modules/customer_management/customer_details.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Table Styles | Slate Admin</title>	
+	
+	<!--Includes menu-->
+        <?php require_once('../../components/external_files.php'); ?>
+		
+		<style>
+			
+		</style>
+</head>
+
+<body>
+	
+<div id="wrapper">
+	
+	<div id="header">
+		<h1><a href="index2.html">Sobol Inc</a></h1>
+		
+		<div id="info">
+			<h4>Welcome Archie</h4>
+			
+			<p>
+				Logged in as Editdmin
+				<br />
+				You have <a href="javascript:;">5 messages</a>
+			</p>
+			
+			<img src="../../images/avatar.jpg" alt="avatar" />
+		</div> <!-- #info -->
+				
+	</div> <!-- #header -->	
+	
+	
+		<!--Includes menu-->
+		<?php require_once('../../components/menu.php'); ?>
+                
+	
+	
+	
+	<div id="content" class="xfluid">
+		
+		<div class="portlet x12">
+			<div class="portlet-header"><h4>Tables</h4></div>
+			
+			<div class="portlet-content">
+				
+				<form action="#" method="post" class="form label-inline">
+	
+					<table>
+						<tr>
+							<td colspan="2" align="left">
+							<div class="field"><label for="fname">Customer ID </label> <input id="fname" name="fname" size="6" type="text" class="medium" /></div>
+							</td>
+						</tr>
+						<tr>
+							<td >
+							<div class="field"><label for="lname">First name </label> <input id="lname" name="lname" size="20" type="text" class="medium" /></div>
+							</td>
+							<td>
+							<div class="field"><label for="lname">Last name </label> <input id="lname" name="lname" size="20" type="text" class="medium" /></div>
+							</td>
+						</tr>
+						<tr>
+							<td>
+							<div class="field"><label for="lname">Email </label> <input id="lname" name="lname" size="20" type="text" class="medium" /></div>
+							</td>
+							<td>
+								<div class="field phone_field"><label for="telephone">Telephone</label> <input id="telephone" size="3" type="text" class="xsmall" /> - <input size="3" type="text" class="xsmall" /> - <input size="4" type="text" class="xsmall" />
+
+								<p class="field_help">(###) - ### - ####</p>
+				
+							</div>
+							</td>
+						</tr>
+						
+						<tr>
+							<td colspan="2">
+							<div class="field"><label for="address1">Address 1 </label> <input id="address1" size="40" type="text" class="large" /></div>
+							</td>
+						</tr>
+						
+						<tr>	
+							<td colspan="2">																
+							<br />
+							<div class="buttonrow">
+								<button class="btn">Save</button>
+							</div>
+							</td>
+						</tr>
+
+						</form>
+						</table>
+
+<br /><br />
+<br /><br />
+				
+				
+				<br />
+				
+				<a name="basic"></a>
+				<h2>Jobs</h2>
+				
+				<table cellpadding="0" cellspacing="0" border="0" class="display">
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Description</th>
+							<th>Creation date</th>
+							<th>Status</th>							
+						</tr>
+					</thead>
+					<tbody>
+						<tr class="odd gradeX">
+							<td>80031</td>							
+							<td>Job 2</td>
+							<td>Replacement of old window</td>
+							<td class="center"> 3/7/2003</td>
+							<td class="center">Completed</td>
+						</tr>
+						<tr class="even gradeC">
+							<td>66398</td>							
+							<td>Job 2</td>
+							<td>Installation of kitchen door</td>
+							<td class="center"> 10/9/2007</td>
+							<td class="center">Completed</td>
+						</tr>
+						<tr class="odd gradeA">
+							<td>863407</td>						
+							<td>Job 2</td>
+							<td>Replacement of all windows and doors</td>
+							<td class="center"> 1/1/2001</td>
+							<td class="center">Pending</td>
+						</tr>
+						<tr class="even gradeA">
+							<td>80031</td>							
+							<td>Job 1</td>
+							<td>IBedroom windows</td>
+							<td class="center"> 7/4/2005</td>
+							<td class="center">Pending</td>
+						</tr>
+						</tbody>
+					</table>
+					
+					<br /><br /><br />
+				
+				
+				
+				
+				
+				
+				
+			
+			</div>
+		</div>
+		
+
+		
+	</div> <!-- #content -->
+	
+	
+	<!--Includes footer-->
+        <?php require_once('../../components/footer.php'); ?>
+	
+</div> <!-- #wrapper -->
+
+	
+</body>
+
+</html>
---
+++ bts530/ui/modules/media_management/gallery.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Sobol Inc</title>	
+	
+	<link rel="stylesheet" href="../../css/screen.css" type="text/css" media="screen" title="no title" charset="utf-8" />	
+	<link rel="stylesheet" href="../../css/plugin.css" type="text/css" media="screen" title="no title" charset="utf-8" />	
+	<link rel="stylesheet" href="../../css/custom.css" type="text/css" media="screen" title="no title" charset="utf-8" />		
+	
+	<style type="text/css" media="screen">
+		
+	</style>
+
+</head>
+
+<body>
+	
+<div id="wrapper" class="clearfix">
+	
+	<div id="top">
+		<div id="header">
+			<h1><a href="index2.html">Sobol Inc</a></h1>
+			
+			<div id="info">
+				<h4>Welcome Archie</h4>
+				
+				<p>
+					Logged in as Admin
+					<br />
+					You have <a href="javascript:;">5 messages</a>
+				</p>
+				
+				<img src="../../images/avatar.jpg" alt="avatar" />
+			</div> <!-- #info -->
+					
+		</div> <!-- #header -->	
+		
+		
+                <!--Includes menu-->
+		<?php require_once('../../components/menu.php'); ?>
+                
+                
+                
+	</div> <!-- #top -->
+	
+	<div id="content" class="xfluid">
+		
+		<a name="line"></a>
+		<div id="gallery_wrapper" class="portlet x8">
+			
+			<div class="portlet-header">
+				<h4>Image Gallery</h4>
+			</div> <!-- .portlet-header -->
+			
+			
+			<div class="portlet-content">
+				
+				<ul class="gallery">
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/crayons_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/crayons_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/coins_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/coins_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+						<li>
+							<img alt="" src="../../images/rain_small.jpg"/>
+							
+							<div class="actions">	
+								<a  class="btn btn-orange btn-small" rel="facebox" href="../../images/rain_large.jpg">View</a>
+								<a href="javascript:;" class="btn btn-grey btn-small delete">Delete</a>
+							</div>
+						</li>
+						
+					</ul>
+
+
+			</div> <!-- .portlet-content -->
+		</div> <!-- .portlet -->
+		
+		
+		<div class="portlet x4">
+			
+			<div class="portlet-header">
+				<h4>Upload File</h4>
+			</div> <!-- .portlet-header -->
+			
+			<div class="portlet-content">
+				
+				<form id="add-user-form" action="" method="get" class="form label-top">
+					
+					<div class="field"><label for="file_name">File Name </label> <input id="file_name" name="file_name" size="30" type="text" value="" /></div>
+					
+					<div class="field clearfix">								
+						<label for="lname">File Upload </label> 								
+						<input style="opacity: 0;" size="35" class="file" type="file" />								
+					</div>
+					
+					<div class="field">
+						<label for="tags">Tags </label>
+						<input id="file_name" name="file_name" size="30" type="text" value="" />							
+					</div>
+					
+					<div class="buttonrow">
+						<button class="btn">Upload File</button>
+					</div> 		
+				
+					
+				</form>
+				
+			</div> <!-- .portlet-content -->
+			
+		</div> <!-- .portlet -->
+
+	</div> <!-- #content -->
+	
+	<div id="footer">
+		
+		<p>Copyright &copy; 2010 <a href="javascript:;">MadeByAmp</a>, all rights reserved.</p>
+		
+	</div> <!-- #footer -->
+	
+</div> <!-- #wrapper -->
+
+<script  type="text/javascript" src="../../js/jquery/jquery.1.4.2.min.js"></script>
+<script  type="text/javascript" src="../../js/slate/slate.js"></script>
+<script  type="text/javascript" src="../../js/slate/slate.portlet.js"></script>
+<script  type="text/javascript" src="../../js/plugin.js"></script>
+
+
+<script type="text/javascript" charset="utf-8">
+$(function () 
+{
+	slate.init ();
+	slate.portlet.init ();	
+});
+</script>
+	
+</body>
+
+</html>
